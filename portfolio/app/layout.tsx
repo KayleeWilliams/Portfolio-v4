6a4426dbef9bdef130a43359d5b18d3e8c872e60
Kaylee <65376239+KayleeWilliams@users.noreply.github.com>
2023-04-11 12:35:44 +0100

    feat: :sparkles: Added social links to footer

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -1,9 +1,13 @@
 "use client";
 
 import { useEffect } from "react";
+import Link from "next/link";
 import "./global.css";
 
 import Navigation from "./Navigation";
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+import { BsMastodon } from "react-icons/bs";
+
 
 export default function RootLayout({
   children,
@@ -25,9 +29,9 @@ export default function RootLayout({
         document.body.scrollTop > 20 ||
         document.documentElement.scrollTop > 20
       ) {
-        navbar.style.background = "hsla(0, 0%, 13%, 1)";
+        navbar.style.background = "hsla(270, 13%, 3%, 1)";
       } else {
-        navbar.style.background = "hsla(0, 0%, 13%, 0)";
+        navbar.style.background = "hsla(270, 13%, 3%, 0)";
       }
     }
 
@@ -43,8 +47,18 @@ export default function RootLayout({
       <body className="bg-black">
         <Navigation />
         <main className="mb-4 min-h-screen">{children}</main>
-        <footer className="bg-[#080709] flex flex-col w-full text-center justify-center align-center h-12 z-auto bottom-0 opacity-100">
-          <p className="text-sm text-white"> Created by Kaylee Williams </p>
+        <footer className="bg-[#080709] flex flex-row justify-center px-8 w-full text-center align-center h-12 z-auto bottom-0 opacity-100">
+          <div className="flex flex-row gap-4">
+            <Link href="http://github.com/KayleeWilliams" target="_blank">
+              <AiFillGithub className="w-8 h-8 text-white hover:text-gray-200 transition delay-75 duration-300 ease-in-out hover:-translate-y-1" />
+            </Link>
+            <Link href="http://linkedin.com/in/kaylee-w/" target="_blank">
+              <AiFillLinkedin className="w-8 h-8 text-white hover:text-gray-200 transition delay-75 duration-300 ease-in-out hover:-translate-y-1 " />
+            </Link>
+            <Link href="http://mastodon.social/@Kayleew" target="_blank">
+              <BsMastodon className="w-8 h-8 text-white hover:text-gray-200 transition delay-75 duration-300 ease-in-out hover:-translate-y-1 " />
+            </Link>
+          </div>
         </footer>
       </body>
     </html>
